31d43e0cf6ecd9f202cce1412a75523f396d990b
bigboidanwithacan <38168108+bigboidanwithacan@users.noreply.github.com>
2025-08-07 01:20:33 -0700

    fixed reload and midway into mvp

--- commands/base/standard.js
+++ commands/base/standard.js
@@ -1,4 +1,6 @@
-import { ButtonBuilder, EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonStyle } from "discord.js";
+import { ButtonBuilder, EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonStyle, ComponentType, MessageFlags } from "discord.js";
+import { Chalk } from "./../../utility/logger.js";
+const logChalk = new Chalk();
 
 export const data = new SlashCommandBuilder()
 	.setName('standard')
@@ -31,7 +33,52 @@ export async function execute(interaction) {
 
 	await interaction.editReply(`${interaction.user} has just initiated a trivia game! Follow the step below to join. Countdown till the trivia game starts <t:${start}:R>`);
 	setTimeout(async () => await interaction.deleteReply(), 30_000);
-	await interaction.channel.send({ content: 'Click the button below to join the game if you are not the current host!', components: [joinRow]});
+
+	const joinMessage = await interaction.channel.send({ 
+		content: 'Click the button below to join the game if you are not the current host!', 
+		components: [joinRow]
+	});
+
+	let players = [interaction.user.id];
+	const joinedMessages = [
+		'You\'re all set! 🎉 No need to click \'Join\' again — you\'re already in the game and ready to roll!',
+		'You\'re in! 🕹️ No need to hit \'Join\' again — just sit tight, the game’s about to begin!',
+		'Already joined — we’re just as excited as you are! 😄',
+		'Hold tight! You\'ve already joined the game. One click is all it takes! 🚀',
+		'Easy does it! You\'re already part of the game. Let the fun begin! 🎮',
+		'Double join? You\'re eager — we love that! But one join is all you need. 😄',
+	]
+	const joinFilter = buttonInteraction => {
+		if (!(players.includes(buttonInteraction.user.id))){
+			return true;
+		}
+		buttonInteraction.reply({
+			content: joinedMessages[Math.round(Math.random()*joinedMessages.length)],
+			flags: MessageFlags.Ephemeral,
+		})
+	};
+
+	const joinButtonCollector = joinMessage.createMessageComponentCollector({
+		filter: joinFilter,
+		componentType: ComponentType.Button,
+		time: 30_000,
+	});
+
+	joinButtonCollector.on('collect', buttonInteraction => {
+		players.push(buttonInteraction.user.id);
+		buttonInteraction.reply({
+			content: 'You have joined the game!',
+			flags: MessageFlags.Ephemeral,
+		})
+	});
+
+	joinButtonCollector.on('end', buttonInteraction => {
+		console.error(buttonInteraction);
+		logChalk.error(buttonInteraction);
+		buttonInteraction.reply('Game is starting!');
+		setTimeout(() => buttonInteraction.deleteReply(), 5_000);
+	})
+
 
 	let counter = 1;
 	for (const fullQuestion of results ) {
--- commands/util/reload.js
+++ commands/util/reload.js
@@ -27,11 +27,11 @@ export async function execute(interaction) {
 
 	try {
 		const commandsFolderPath = path.join(__dirname);
-		// i haven't found a better way to truncate the current file name and folder name from the path
+		// i haven't found a better way to truncate the current folder name from the path
 		const correctFolderPath = path.join(commandsFolderPath.slice(0, -5));
 		const commandsFolder = readdirSync(correctFolderPath);
 		let correctFolder = '';
-		for (const folder of commandsFolder){
+		for (const folder of commandsFolder) {
 			const currentPathCheck = path.join(correctFolderPath, folder, `${commandName}.js`);
 			if (existsSync(currentPathCheck)) {
 				correctFolder = folder;
@@ -41,7 +41,7 @@ export async function execute(interaction) {
 		}
 		const commandPath = path.join(correctFolderPath, correctFolder,`${commandName}.js`);
 		 // Convert to a file URL and add a query to bypass the cache
-		const commandUrl = `${pathToFileURL(commandPath).href}?update=${Date}`;
+		const commandUrl = `${pathToFileURL(commandPath).href}?update=${Date.now()}`;
 
 		// dynamically import the command
 		const newCommandModule = await import(commandUrl);
